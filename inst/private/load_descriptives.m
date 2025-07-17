4fbe0efde30202109bc909a4c4c05a27e60d6d63
pr0m1th3as <abertsatos@biol.uoa.gr>
2025-07-18 01:17:52 +0300

    update private function for loading stats

--- inst/private/load_descriptives.m
+++ inst/private/load_descriptives.m
@@ -14,10 +14,17 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {csg-toolkit} {[@var{idx}, @var{mu}, @var{sigma}, @var{Mlo}, @
-## @var{Mhi}, @var{Flo}, @var{Fhi}] =} load_descriptives (@var{bone})
+## @deftypefn  {csg-toolkit} {@var{varargout} = } load_descriptives (@var{bone}, @var{type})
+## @deftypefnx {csg-toolkit} {[@var{idx}, @var{mu}, @var{sigma}, @var{Mlo}, @
+## @var{Mhi}, @var{Flo}, @var{Fhi}] =} load_descriptives (@var{bone}, @qcode{'sex'})
+## @deftypefnx {csg-toolkit} {[@var{mu}, @var{sigma}, @var{lower}, @var{upper}, @
+## @var{idx}] =} load_descriptives (@var{bone}, @qcode{'pair'})
 ##
-## This function returns the descriptives related to @var{bone}.
+## This function returns the descriptives related to @var{bone} according to the
+## requested @var{type}, which can be either @qcode{'sex'} or @qcode{'pair'}.
+##
+## When @var{type} is @qcode{'sex'}, the following output arguments, which are
+## utilized by the @qcode{longbone_Sex} function, are returned:
 ##
 ## @itemize
 ## @item @var{idx} contains the indices to @var{DATA} used by the classifiers.
@@ -34,41 +41,86 @@
 ## outliers for females.
 ## @end itemize
 ##
+## When @var{type} is @qcode{'pair'}, the following output arguments, which are
+## utilized by the @qcode{longbone_Pair} function, are returned:
+##
+## @itemize
+## @item @var{mu} contains the mean differences of absolute values between left
+## and right sides for each measurement.
+## @item @var{sigma} contains the standard deviations of the differences of the
+## absolute values between left and right sides for each measuremens.
+## @item @var{lower} contains the lower threshold below which a tested pair can
+## be considered a definite mismatch.
+## @item @var{upper} contains the upper threshold above which a tested pair can
+## be considered a definite mismatch.
+## @item @var{idx} contains a logical vector of the measuremens to be used in
+## pair matching.
+## @end itemize
+##
 ## @end deftypefn
 
-function [idx, mu, sigma, Mlo, Mhi, Flo, Fhi] = load_descriptives (bone)
-  D = load (fullfile (pwd, "private", 'descriptives.mat'));
-  if (strcmpi (bone, "Femur"))
-    idx = D.Fvars(:,1)';
-    mu = D.Fvars(:,2)';
-    sigma = D.Fvars(:,3)';
-    Mlo = D.Fvars(:,4)';
-    Mhi = D.Fvars(:,5)';
-    Flo = D.Fvars(:,6)';
-    Fhi = D.Fvars(:,7)';
-  elseif (strcmpi (bone, "Humerus"))
-    idx = D.Hvars(:,1)';
-    mu = D.Hvars(:,2)';
-    sigma = D.Hvars(:,3)';
-    Mlo = D.Hvars(:,4)';
-    Mhi = D.Hvars(:,5)';
-    Flo = D.Hvars(:,6)';
-    Fhi = D.Hvars(:,7)';
-  elseif (strcmpi (bone, "Tibia"))
-    idx = D.Tvars(:,1)';
-    mu = D.Tvars(:,2)';
-    sigma = D.Tvars(:,3)';
-    Mlo = D.Tvars(:,4)';
-    Mhi = D.Tvars(:,5)';
-    Flo = D.Tvars(:,6)';
-    Fhi = D.Tvars(:,7)';
-  elseif (strcmpi (bone, "Ulna"))
-    idx = D.Uvars(:,1)';
-    mu = D.Uvars(:,2)';
-    sigma = D.Uvars(:,3)';
-    Mlo = D.Uvars(:,4)';
-    Mhi = D.Uvars(:,5)';
-    Flo = D.Uvars(:,6)';
-    Fhi = D.Uvars(:,7)';
+function [varargout] = load_descriptives (bone, type)
+  if (strcmpi (type, 'sex'))
+    D = load (fullfile (pwd, 'private', 'descriptives_sex.mat'));
+    if (strcmpi (bone, 'Femur'))
+      varargout{1} = D.Fvars(:,1)';
+      varargout{2} = D.Fvars(:,2)';
+      varargout{3} = D.Fvars(:,3)';
+      varargout{4} = D.Fvars(:,4)';
+      varargout{5} = D.Fvars(:,5)';
+      varargout{6} = D.Fvars(:,6)';
+      varargout{7} = D.Fvars(:,7)';
+    elseif (strcmpi (bone, 'Humerus'))
+      varargout{1} = D.Hvars(:,1)';
+      varargout{2} = D.Hvars(:,2)';
+      varargout{3} = D.Hvars(:,3)';
+      varargout{4} = D.Hvars(:,4)';
+      varargout{5} = D.Hvars(:,5)';
+      varargout{6} = D.Hvars(:,6)';
+      varargout{7} = D.Hvars(:,7)';
+    elseif (strcmpi (bone, 'Tibia'))
+      varargout{1} = D.Tvars(:,1)';
+      varargout{2} = D.Tvars(:,2)';
+      varargout{3} = D.Tvars(:,3)';
+      varargout{4} = D.Tvars(:,4)';
+      varargout{5} = D.Tvars(:,5)';
+      varargout{6} = D.Tvars(:,6)';
+      varargout{7} = D.Tvars(:,7)';
+    elseif (strcmpi (bone, 'Ulna'))
+      varargout{1} = D.Uvars(:,1)';
+      varargout{2} = D.Uvars(:,2)';
+      varargout{3} = D.Uvars(:,3)';
+      varargout{4} = D.Uvars(:,4)';
+      varargout{5} = D.Uvars(:,5)';
+      varargout{6} = D.Uvars(:,6)';
+      varargout{7} = D.Uvars(:,7)';
+    endif
+  elseif (strcmpi (type, 'pair'))
+    D = load (fullfile (pwd, 'private', 'descriptives_pair.mat'));
+    if (strcmpi (bone, 'Femur'))
+      varargout{1} = D.Fvars(1,:);
+      varargout{2} = D.Fvars(2,:);
+      varargout{3} = D.Fvars(3,:);
+      varargout{4} = D.Fvars(4,:);
+      varargout{5} = logical (D.Fvars(5,:));
+    elseif (strcmpi (bone, 'Humerus'))
+      varargout{1} = D.Hvars(1,:);
+      varargout{2} = D.Hvars(2,:);
+      varargout{3} = D.Hvars(3,:);
+      varargout{4} = D.Hvars(4,:);
+      varargout{5} = logical (D.Hvars(5,:));
+    elseif (strcmpi (bone, 'Tibia'))
+      varargout{1} = D.Tvars(1,:);
+      varargout{2} = D.Tvars(2,:);
+      varargout{3} = D.Tvars(3,:);
+      varargout{4} = D.Tvars(4,:);
+      varargout{5} = logical (D.Tvars(5,:));
+    elseif (strcmpi (bone, 'Ulna'))
+      varargout{1} = D.Uvars(1,:);
+      varargout{2} = D.Uvars(2,:);
+      varargout{3} = D.Uvars(3,:);
+      varargout{4} = D.Uvars(4,:);
+      varargout{5} = logical (D.Uvars(5,:));
+    endif
   endif
 endfunction
